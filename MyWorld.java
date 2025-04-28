8a2d0b49a6f198f4411656adfd792f62ed2eeb05
CC-iam <1202manman@gmail.com>
2025-04-28 14:12:54 -0400

    Add elephant

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,15 @@
 import greenfoot.*;
-
+/**
+ * The World our hero lives in. 
+ * 
+ * @author Carmen Cheung
+ * @version Apr 28, 2025
+ */
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200); 
     }
 }
