2a383f9be0d36857d82177ee37e754471f7e4092
CC-iam <1202manman@gmail.com>
2025-04-28 14:44:52 -0400

    Update world

--- MyWorld.java
+++ MyWorld.java
@@ -6,13 +6,46 @@ import greenfoot.*;
  * @version Apr 28, 2025
  */
 public class MyWorld extends World {
+    public int score =0;
+    Label scoreLabel;
     public MyWorld() {
-        super(600, 400, 1);
-        
+        super(600, 400, 1, false);
+        //Create the elephant object
         Elephant elephant = new Elephant();
-        addObject(elephant, 300, 200); 
+        addObject(elephant, 300, 200);  
         
+        //Create a label
+        scoreLabel = new Label(0,80);
+        addObject(scoreLabel, 50, 50); 
+        
+        createApple(); 
+    }
+    
+    /** 
+     * End the game and draw "Game Over"
+     */
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200); 
+    }
+    
+    /**
+     * Increase score
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
+    /**
+     * Create an new apple at random location at top of screen
+     */
+    public void createApple()
+    {
         Apple apple = new Apple();
-        addObject(apple, 300, 0); 
+        int x = Greenfoot.getRandomNumber(600); 
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
